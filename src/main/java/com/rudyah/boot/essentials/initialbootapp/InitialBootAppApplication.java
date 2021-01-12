e5a9018581caa7602a4bca066e941d5641fc9d70
Rudy Hernandez Jr <rudolpharthur@gmail.com>
2021-01-12 13:49:11 -0800

    Initial commit

---
+++ src/main/java/com/rudyah/boot/essentials/initialbootapp/InitialBootAppApplication.java
@@ -0,0 +1,15 @@
+package com.rudyah.boot.essentials.initialbootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class InitialBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InitialBootAppApplication.class, args);
+	}
+}
